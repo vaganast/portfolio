41f0fefc5976e5e0cc522059be621bcef2aae9d5
vaganast <71605687+vaganast@users.noreply.github.com>
2022-05-21 22:57:26 +0300

    Sanity React Higher Order Component
    dynamic fetch about section ,
    hoc to header and about

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
-import { images } from '../../constants'
-
-
+import AppWrap from '../../wrapper/AppWrap'
+import { urlFor, client } from '../../client'
 import './About.scss'
 
-const abouts=[
-  { title:'Web Development', description:'I am a good development', imgUrl: images.about01 },
-  { title:'Frontend Development', description:'I am a good development', imgUrl: images.about02},
-  { title:'Material Ui', description:'I am a good development', imgUrl: images.about03},
-  { title:'E-Commerce', description:'I am a good development', imgUrl: images.about04},
-]
+
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';   
+    
+    client.fetch(query)
+    .then((data) =>{setAbouts(data)})
+  }, []);
+  
+
   return (
     <>
       <h2 className='head-text'>
@@ -32,7 +37,7 @@ const About = () => {
             className='app__profile-item'
             key={about.title + index}
             >
-              <img src={about.imgUrl} alt={about.title} />
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
               <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
 
@@ -43,4 +48,4 @@ const About = () => {
   )
 }
 
-export default About
+export default AppWrap(About, 'about' )
--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 
+
+import AppWrap from '../../wrapper/AppWrap'
 import { images } from '../../constants'
 
 import './Header.scss'
@@ -18,7 +20,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div id='home' className='app__header app__flex'>
+    <div  className='app__header app__flex'>
       <motion.div 
       whileInView={{ x: [-100, 0], opacity: [0, 1] }} 
       transition={{ duration: 2.5 }}
@@ -69,4 +71,4 @@ const Header = () => {
   )
 }
 
-export default Header
+export default AppWrap(Header, 'home' )
